4a9d0e1000e853f079b1628adada602ea65741f7
acaibawl <hagiya0719@gmail.com>
2025-03-02 02:30:26 +0900

    page指定で開いてる場合の画面タイトルを追加

--- app/pages/page/[page].vue
+++ app/pages/page/[page].vue
@@ -4,6 +4,17 @@ import ArticleList from '~/components/ArticleList.vue';
 const route = useRoute();
 const pageParam = route.params.page;
 const pageNumber = parseInt(pageParam as string) || 1;
+
+useHead({
+  title: `記事一覧 - ${pageNumber}ページ`,
+  meta: [
+    {
+      hid: 'description',
+      name: 'description',
+      content: '記事一覧ページです。'
+    }
+  ]
+})
 </script>
 
 <template>
